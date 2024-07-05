6a42cfe5e2f0d107979bf9ddc4236256d6b46de5
Rudra <rudrahimangshoo2020@gmail.com>
2024-07-05 22:12:02 +0600

    build-issue-fix

---
+++ src/components/product-page/ProductSelect.tsx
@@ -0,0 +1,33 @@
+// import { useState } from "react";
+// import CustomRadioButton from "../ui/SharedComponent/CustomRadioButton";
+
+// const ProductSelect = () => {
+//     const [selectedValue, setSelectedValue] = useState<string>("");
+//   return (
+//     <>
+//     <div className=" devid-primary-gray devide-y " >
+//     </div>
+//     <div className="flex flex-row space-x-4   ">
+//     </div>
+//     <div className="flex space-x-4">
+//     <CustomRadioButton
+//       checked={selectedValue === "option1"}
+//       onChange={() => setSelectedValue("option1")}
+//       label="label"
+//     />
+//     <CustomRadioButton
+//       checked={selectedValue === "option2"}
+//       onChange={() => setSelectedValue("option2")}
+//       label="label"
+//     />
+//     <CustomRadioButton
+//       checked={selectedValue === "option3"}
+//       onChange={() => setSelectedValue("option3")}
+//       label="label"
+//     />
+//   </div>
+//   </>
+//   );
+// };
+
+// export default ProductSelect;
